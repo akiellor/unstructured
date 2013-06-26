3ed73ec394f940f804fc05aac7f2177795466806
Andrew Kiellor <akiellor@gmail.com>
2013-06-25 17:42:24 -0700

    Renamed State to Status. Only allows changes in default status.

--- example/src/main/java/unstructured/example/Order.java
+++ example/src/main/java/unstructured/example/Order.java
@@ -20,7 +20,45 @@ public class Order{
     }
 
     public Order change(Unstructured changes) {
-        return new Order(unstructured.merge(changes.reject(Lists.<Address>newArrayList(new Address("id")))));
+        return state().change(changes.reject(Lists.<Address>newArrayList(
+                new Address("id"), new Address("buyer"), new Address("seller"))));
+    }
+
+    private Status state(){
+        Object status = unstructured.get("status");
+        if(Status.Settled.isSettled(status)){
+            return new Status.Settled();
+        }else{
+            return new Status.Default(unstructured);
+        }
+    }
+
+    private interface Status {
+        Order change(Unstructured changes);
+
+        class Default implements Status {
+            private final Unstructured unstructured;
+
+            public Default(Unstructured unstructured){
+                this.unstructured = unstructured;
+            }
+
+            @Override public Order change(Unstructured changes) {
+                return new Order(unstructured.merge(changes));
+            }
+        }
+
+        class Settled implements Status {
+            private static final String NAME = "SETTLED";
+
+            public static boolean isSettled(Object status) {
+                return NAME.equals(status);
+            }
+
+            @Override public Order change(Unstructured changes) {
+                throw new IllegalStateException();
+            }
+        }
     }
 
     @Override
--- example/src/test/java/unstructured/example/OrderTest.java
+++ example/src/test/java/unstructured/example/OrderTest.java
@@ -1,13 +1,17 @@
 package unstructured.example;
 
 import com.google.common.collect.ImmutableMap;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import unstructured.Unstructured;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class OrderTest {
+    @Rule public ExpectedException exception = ExpectedException.none();
+
     @Test
     public void shouldNotAllowChangesToId() {
         Order order = new Order(new Unstructured());
@@ -15,5 +19,32 @@ public class OrderTest {
         Order newOrder = order.change(new Unstructured(ImmutableMap.<Object, Object>of("id", 9)));
 
         assertThat(newOrder, equalTo(order));
+    }
+
+    @Test
+    public void shouldNotAllowChangesToBuyer() {
+        Order order = new Order(new Unstructured());
+
+        Order newOrder = order.change(new Unstructured(ImmutableMap.<Object, Object>of("buyer", 9)));
+
+        assertThat(newOrder, equalTo(order));
+    }
+
+    @Test
+    public void shouldNotAllowChangesToSeller() {
+        Order order = new Order(new Unstructured());
+
+        Order newOrder = order.change(new Unstructured(ImmutableMap.<Object, Object>of("seller", 9)));
+
+        assertThat(newOrder, equalTo(order));
+    }
+
+    @Test
+    public void shouldNotAllowChangesWhenSettled() {
+        exception.expect(IllegalStateException.class);
+
+        Order order = new Order(new Unstructured(ImmutableMap.<Object, Object>of("status", "SETTLED")));
+
+        order.change(new Unstructured(ImmutableMap.<Object, Object>of()));
     }
 }
